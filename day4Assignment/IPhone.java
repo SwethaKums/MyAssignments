6f804af5b5a9498a7e4ca498be320f937d12598d
SwethaKums <104850290+SwethaKums@users.noreply.github.com>
2022-12-02 21:01:27 +0530

    Delete IPhone.java

--- day4Assignment/IPhone.java
+++
@@ -1,9 +0,0 @@
-package week2.day4Assignment;
-
-public abstract class IPhone implements Mobile{
-	public  void iPhone1() {
-		System.out.println("this is iphone");
-	}
-	public abstract void iPhone2();
-
-}
